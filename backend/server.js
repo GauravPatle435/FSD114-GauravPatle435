85b4f3f16028b9f36d66a7bb5d826678e4d99e22
GauravPatle435 <gauravpatle435@gmail.com>
2026-02-09 12:28:31 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -53,3 +54,13 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/build/index.html")
+    );
+  });
+}
